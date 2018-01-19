6bbe5a2d600ce615dd4cd2a8cf8bf97696b6b133
elif tekin <elif tekin>
2018-01-19 16:28:43 +0300

    Add project files.

---
+++ todoList/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(todoList.Startup))]
+namespace todoList
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
